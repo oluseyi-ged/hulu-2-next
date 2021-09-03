038cc249d7610276678e5d00df93561bc22abfee
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-03 10:27:22 +0100

    Flipmove added for smooth transition

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
 import Image from "next/image"
 import { ThumbUpIcon } from "@heroicons/react/outline"
+import { forwardRef } from "react"
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/"
 
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -27,6 +31,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   )
-}
+})
 
 export default Thumbnail
