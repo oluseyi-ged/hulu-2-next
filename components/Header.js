44ec691964785e15cbd20aa12b7da155e6e9e361
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-03 02:47:33 +0100

    implementing tailwind css

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
+import Image from "next/image"
+
 function Header() {
   return (
     <header className="">
       <h1>The header</h1>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
     </header>
   )
 }
