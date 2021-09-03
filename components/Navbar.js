fd3b58aa83ed7ed8164822cac0acb0c4601dfaea
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-03 09:32:20 +0100

    router added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,16 @@
 import requests from "../utils/requests"
+import { useRouter } from "next/router"
 
 function Navbar() {
+  const router = useRouter()
+
   return (
     <nav className="relative">
       <div className="flex sm:px-20 px-10 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
+            onClick={() => router.push(`/?genre=${key}`)}
             className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
